7f124b4e729619ddda24b8c313eaad3db728c817
nealterrell <neal.terrell@csulb.edu>
2014-12-15 10:51:46 -0800

    Initial commit.

---
+++ Lectures/Introductions/Day1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x = 5;
+   int arr[] = {1, 2, 3, 4};
+   
+   cout << "The variable x has value " << x << endl;
+
+   arr[6] = 10;
+
+   cout << "After changing a value in arr, the variable x now has value " << x << endl;
+
+   if (x != 5) {
+       cout << "???" << endl;
+   }
+}
