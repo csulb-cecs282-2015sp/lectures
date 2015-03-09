035d6561c7517788979b77b031878626442e3287
nealterrell <neal.terrell@csulb.edu>
2015-03-09 13:25:41 -0700

    Revamp.

--- Lectures/Classes/Dynamic Memory/heap.cpp
+++ Lectures/Classes/Dynamic Memory/heap.cpp
@@ -5,76 +5,89 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
    int x = 10;
-   // where is x in memory?
-   // when is x removed from memory?
-   // who governs this?
+   /*
+   Where is x in memory?
+   How many int variables do I get when I declare x?
+   When is x removed from memory?
+   Who governs this?
+   These answers together form "static memory management": rules for managing
+   memory for variables whose demands do not change past compile time.
+   */
 
 
+   /*
+   Some memory patterns require us to break these rules. Sometimes we need
+   variables that "live" outside the scope they were constructed in.
+   Sometimes we don't know how much memory we will need at compile time.
+   Sometimes we want explicit control over when to free the memory for a
+   variable. This requires a second location for storing variables, called
+   "the heap".
+   */
+
+   /*
+   The "new" keyword is used in C++ AND in Java to declare a variable on the
+   heap. Combine "new" with the name of a type to create a variable of that type
+   on the heap. "new" gives back to you a POINTER to the heap variable.
+   */
+   int *heapInt = new int(10); // inside the parentheses: initial value
+
+   /*
+   heapInt is a pointer on the stack; it gets 4 bytes of space. It POINTS TO
+   4 bytes for an integer on the heap. We do not know where this space will be
+   when we run the program, only that it is not on the stack. heapInt itself
+   will be popped when it goes out of scope, but the memory it points to will
+   not be freed until we explicitly say to.
+   */
 
 
-
-   // the stack is not the only memory location for variables.
-   // what if I want a variable that does NOT get removed when it goes 
-   // out of scope?
-
-   // the "new" keyword can create a variable of a certain type in a
-   // memory location called the heap.
-   // when using new, you get back a pointer to this heap location.
-
-   int *heapInt = new int(10);
-   // heapInt points to an arbitrary heap position containing a value of 10.
-   // how does the heap decide *how much* memory to give you?
-
-
-
-
-
-   // we can use heap pointers like any other pointer.
+   // We can use heap pointers like any other pointer.
    cout << "The value on the heap is " << *heapInt << endl;
    *heapInt = 100;
    cout << "Now it is " << *heapInt << endl;
 
+   /*
+   THE MOST IMPORTANT THING TO KNOW ABOUT THE HEAP:
+   If you new it, you must make sure that someone deletes it
 
-   // we can do this with any variable type, even class types.
-   string *heapString = new string("No reason to do this, just an example");
-   cout << *heapString << endl;
+   As soon as you know you are done with a heap variable, you can "free" its
+   memory using the delete keyword.
+   */
+   delete heapInt; // done with heapInt, free its memory.
+
+   /*
+   This DOES NOT remove the heapInt POINTER FROM THE STACK; it only "marks"
+   the heap space that heapInt POINTS TO as "freed", meaning that someone else
+   may come along and use that space for something else.
+   heapInt still exists and still points to the "freed" location, making for
+   subtle bugs:
+   */
+   *heapInt = 500;
+   // WARNING: the 4 bytes heapInt points to was deleted; I am now writing to 
+   // memory that I do not own.
+
+   // To prevent errors like above, set a pointer to nullptr after deleting it:
+   heapInt = nullptr;
 
 
-   // THE MOST IMPORTANT THING TO KNOW ABOUT THE HEAP:
-   // if you new it, you must make sure that someone deletes it
-
-   // as soon as you know you are done with a heap variable, you can "free" its
-   // memory using the delete keyword.
-   delete heapString; // done with heapString, free its memory.
-
-   // what happens if I don't delete it?
+   // What happens if I don't delete heapInt?
 
 
 
 
 
+
+
+   // Suppose we create a new int for heapInt to point to.
+   heapInt = new int(100);
 
 
    // I can also allocate a dynamically-sized array with new. !!!
    int* heapArray = new int[*heapInt];
-   // array's size is not known at compile time, so it cannot go on teh stack.
-   // the heap, though, can grab a dynamic amount of memory at run time!
-
+   // The array's size is not known at compile time, so it cannot go on the 
+   // stack. The heap, though, can grab a dynamic amount of memory at run time!
    heapArray[0] = 100;
    cout << heapArray[0] << endl; // etc
 
    // to delete a dynamic array, you MUST use delete[]
    delete[] heapArray;
-
-
-   
-   system("pause");
-   // demonstrate a memory leak. DON'T DO THIS.
-   int total = 0;
-   for (int i = 0; i < 1000000; i++) {
-      int *p = new int(pow(i, 2));
-      total += *p;
-      delete p;
-   }
-   system("pause");
 }
